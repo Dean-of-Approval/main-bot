e7bc760fcb5ef774a8347fa102d3c01e5b270497
Drichter <dean.richterent@gmail.com>
2022-10-08 11:02:17 -0500

    Team staff toggle command

---
+++ src/commands/teamstaff.command.ts
@@ -0,0 +1,10 @@
+import Client from "../struct/Client.js"
+import Args from "../struct/Args.js"
+import Command from "../struct/Command.js"
+import GuildMember from "../struct/discord/GuildMember.js"
+import CommandMessage from "../struct/CommandMessage.js"
+import {
+    formatTimestamp,
+    humanizeArray,
+    humanizeConstant
+} from "@buildtheearth/bot-utils"
